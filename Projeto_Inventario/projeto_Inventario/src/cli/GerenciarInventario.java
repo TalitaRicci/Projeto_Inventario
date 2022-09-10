b8559115c275e697ab48a47a8be65a62b2777d20
Talita Ricci <113118326+TalitaRicci@users.noreply.github.com>
2022-09-09 21:48:30 -0300

    Add files via upload

--- Projeto_Inventario/projeto_Inventario/src/cli/GerenciarInventario.java
+++ Projeto_Inventario/projeto_Inventario/src/cli/GerenciarInventario.java
@@ -1,7 +1,91 @@
 package cli;
 
+import model.Produto;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class GerenciarInventario {
+    public List<Produto> listaProduto = new ArrayList<>();
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        GerenciarInventario gerenciar = new GerenciarInventario();
+        Scanner scan = new Scanner(System.in);
+        int opcao = 0;
+
+        do {
+            System.out.println("MENU");
+            System.out.println("1. Cadastrar Produto");
+            System.out.println("2. Repor Estoque");
+            System.out.println("3. Retirada de Estoque");
+            System.out.println("4. Listar Produto Por Codigo");
+            System.out.println("5. Listar todos os Produtos");
+            System.out.println("9. Sair");
+            System.out.println("Digite sua opcao: ");
+            opcao = Integer.parseInt(scan.nextLine());
+
+            switch (opcao) {
+                case 1:
+                    gerenciar.execCadastrarProduto(scan);
+                    break;
+                case 2:
+                    gerenciar.execReposicao(scan);
+                    break;
+//                case 3:
+//                    gerenciar.execRetirada(scan);
+//                    break;
+//                case 4:
+//                    gerenciar.execListarCodigo(scan);
+//                    break;
+//                case 5:
+//                    gerenciar.execListarTodos();
+//                    break;
+                case 9:
+                    System.out.println("FIM");
+                    break;
+                default:
+                    System.out.println("Opcao invalida!");
+                    break;
+            }
+
+        } while (opcao != 9);
+    }
+        public void execCadastrarProduto(Scanner scan) {
+                Produto produto = new Produto();
+            System.out.println("Digite o código do produto: ");
+            int codigo = Integer.parseInt(scan.nextLine());
+
+            if(!codigoExiste(codigo)){
+                produto.setProdutoCodigo(codigo);
+            }else{
+                System.out.println("Código já existente!");
+                return;
+            }
+
+            System.out.println("Digite o nome do produto: ");
+            produto.setNome(scan.nextLine());
+            System.out.println("Digite o valor do produto: ");
+            produto.setValor(Double.parseDouble(scan.nextLine()));
+            System.out.println("Digite a quantidade: ");
+            produto.setQuantidade(Double.parseDouble(scan.nextLine()));
+            System.out.println("Digite a marca do produto:" );
+            produto.setMarca(scan.nextLine());
+
+            listaProduto.add(produto);
+            System.out.println("Produto cadastrado com sucesso");
+        }
+
+        public void execReposicao(Scanner scan){
+        
+        }
+
+    private boolean codigoExiste(int codigo) {
+        for (Produto produto : listaProduto){
+            if(produto.getProdutoCodigo() == codigo){
+                return true;
+            }
+        }
+        return false;
     }
 }
--- Projeto_Inventario/projeto_Inventario/src/model/Produto.java
+++ Projeto_Inventario/projeto_Inventario/src/model/Produto.java
@@ -2,5 +2,73 @@ package model;
 
 public class Produto {
     private int produtoCodigo;
+    private String nome;
+    private double valor;
+    private double quantidade;
+    private String marca;
 
+    public int getProdutoCodigo() {
+        return produtoCodigo;
+    }
+
+    public void setProdutoCodigo(int produtoCodigo) {
+        this.produtoCodigo = produtoCodigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public double getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(double quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    @Override
+    public String toString() {
+        return "Código Produto = " + produtoCodigo + "\n" +
+                "Nome Produto = " + nome + "\n" +
+                "Valor Produto = " + valor + "\n" +
+                "Quantidade = " + quantidade + "\n" +
+                "Marca do Produto = " + marca + "\n";
+    }
+
+    public boolean repor(double qtd){
+        if(qtd > 0){
+            quantidade += qtd;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean remover(double qtd){
+        if(quantidade <= qtd){
+            qtd -= quantidade;
+            return true;
+        }
+        return false;
+    }
 }
